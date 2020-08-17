d4e4dff983d39bf14f1c46d77f366fe4cf1906c8
Jacek <jackad@o2.pl>
2020-08-17 20:05:51 +0200

    styling provided

--- src/components/CurrencyDisplay/CurrencyDisplay.jsx
+++ src/components/CurrencyDisplay/CurrencyDisplay.jsx
@@ -2,16 +2,19 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { currencyOptions } from './../../constants/currencyConstants';
 import { getRates } from './../../actions/currencyActions';
+import useStyles from './currencyDisplay.style';
+import SyncIcon from '@material-ui/icons/Sync';
 
 const CurrencyDisplay = () => {
   const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState({});
-  const [to, setTo] = useState({});
+  const [from, setFrom] = useState({ name: '' });
+  const [to, setTo] = useState({ name: '' });
   const [rate, setRate] = useState(0);
+
+  const classes = useStyles();
 
   const handleChangeAmount = (event) => {
     event.target.value >= 0 && setAmount(event.target.value);
@@ -29,13 +32,16 @@ const CurrencyDisplay = () => {
     let tempValue = from;
     setFrom(to);
     setTo(tempValue);
+    setRate(1 / rate);
   };
 
   const handleCalculateRates = async () => {
-    const result = await getRates(from.symbol, to.symbol);
+    if (from.symbol && to.symbol && amount > 0) {
+      const result = await getRates(from.symbol, to.symbol);
 
-    result.success &&
-      setRate(result.rates[to.symbol] / result.rates[from.symbol]);
+      result.success &&
+        setRate(result.rates[to.symbol] / result.rates[from.symbol]);
+    }
   };
 
   const getCalculatedAmount = (amount) => Math.round(amount * rate * 100) / 100;
@@ -48,58 +54,61 @@ const CurrencyDisplay = () => {
 
   return (
     <div>
-      <Container maxWidth="sm">
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="textPrimary"
-        >
-          Currency Calculator
-        </Typography>
-        <div>
-          <TextField
-            label="Amount"
-            value={amount}
-            onChange={handleChangeAmount}
-            type="number"
-            variant="outlined"
-          />
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="textPrimary"
+      >
+        Currency Calculator
+      </Typography>
+      <div className={classes.inputContainer}>
+        <TextField
+          label="Amount"
+          value={amount}
+          onChange={handleChangeAmount}
+          type="number"
+          variant="outlined"
+          className={classes.numberInput}
+        />
 
-          <Autocomplete
-            options={currencyOptions}
-            getOptionLabel={(option) => option.name}
-            inputValue={from.name}
-            onChange={handleChangeFrom}
-            renderInput={(params) => (
-              <TextField {...params} label="From" variant="outlined" />
-            )}
-          />
-          <Button onClick={handleSwap}>Swap</Button>
-          <Autocomplete
-            options={currencyOptions}
-            getOptionLabel={(option) => option.name}
-            inputValue={to.name}
-            onChange={handleChangeTo}
-            renderInput={(params) => (
-              <TextField {...params} label="To" variant="outlined" />
-            )}
-          />
-          <Button onClick={handleCalculateRates}>calculate</Button>
+        <Autocomplete
+          options={currencyOptions}
+          getOptionLabel={(option) => option.name}
+          inputValue={from.name}
+          onChange={handleChangeFrom}
+          className={classes.autocompleteInput}
+          renderInput={(params) => (
+            <TextField {...params} label="From" variant="outlined" />
+          )}
+        />
+        <Button onClick={handleSwap} className={classes.swapButton}>
+          <SyncIcon />
+        </Button>
+        <Autocomplete
+          options={currencyOptions}
+          getOptionLabel={(option) => option.name}
+          inputValue={to.name}
+          onChange={handleChangeTo}
+          className={classes.autocompleteInput}
+          renderInput={(params) => (
+            <TextField {...params} label="To" variant="outlined" />
+          )}
+        />
+        <Button onClick={handleCalculateRates}>calculate</Button>
+      </div>
+      {rate !== 0 && (
+        <div className={classes.data}>
+          <Typography variant="body1">{`${amount} ${
+            from.name
+          } = ${getCalculatedAmount(amount)} ${to.name}`}</Typography>
+          <Typography variant="body2">
+            {`1 ${from.symbol} = ${getCalculatedAmount(1)} ${
+              to.symbol
+            } on ${getToday()} `}
+          </Typography>
         </div>
-        {rate !== 0 && (
-          <div>
-            <div>{`${amount} ${from.name} = ${getCalculatedAmount(amount)} ${
-              to.name
-            }`}</div>
-            <div>
-              {`1 ${from.symbol} = ${getCalculatedAmount(1)} ${
-                to.symbol
-              } on ${getToday()} `}
-            </div>
-          </div>
-        )}
-      </Container>
+      )}
     </div>
   );
 };
---
+++ src/components/CurrencyDisplay/currencyDisplay.style.js
@@ -0,0 +1,24 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+export default makeStyles({
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: '150px',
+    justifyContent: 'center',
+  },
+  numberInput: {
+    width: '100px',
+    marginRight: '10px',
+  },
+  autocompleteInput: {
+    width: '250px',
+    marginRight: '10px',
+  },
+  swapButton: {
+    marginRight: '10px',
+  },
+  data: {
+    marginTop: '50px',
+  },
+});

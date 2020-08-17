f1a212417f43b1bfbba1ee805575d26b12a02f72
Jacek <jackad@o2.pl>
2020-08-17 17:59:13 +0200

    Basic container implemented

---
+++ src/components/CurrencyDisplay/CurrencyDisplay.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const CurrencyDisplay = () => {
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Currency Calculator
+        </Typography>
+        <div>
+          <TextField />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CurrencyDisplay;
